43eaee036df6e9da9b008b42b3bf0df78c3a4d70
Erke Xia <exia@andrew.cmu.edu>
2024-10-07 09:48:43 -0400

    support the query 'what is you Andrew ID' and test

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -5,6 +5,11 @@ export default function QueryProcessor(query: string): string {
       "English poet, playwright, and actor, widely regarded as the greatest " +
       "writer in the English language and the world's pre-eminent dramatist."
     );
+  };
+  if (query.toLowerCase().includes("andrew id")) {
+    return (
+      "My Andrew ID is exia."
+    );
   }
 
   return "";
