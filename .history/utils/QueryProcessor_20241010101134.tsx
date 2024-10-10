7a787526e69a0cbcd7a407596edbb8826c1bebfc
Erke Xia <exia@andrew.cmu.edu>
2024-10-10 10:11:44 -0400

    add plus

---
+++ .history/utils/QueryProcessor_20241010101134.tsx
@@ -0,0 +1,33 @@
+export default function QueryProcessor(query: string): string {
+  if (query.toLowerCase().includes("shakespeare")) {
+    return (
+      "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
+      "English poet, playwright, and actor, widely regarded as the greatest " +
+      "writer in the English language and the world's pre-eminent dramatist."
+    );
+  };
+  if (query.toLowerCase().includes("andrew id")) {
+    return (
+      "My Andrew ID is exia."
+    );
+  }
+  if (query.toLowerCase().includes("name")) {
+    return (
+      "Jason"
+    );
+  }
+
+  if (query.toLowerCase().includes("plus")) {
+    const addMatch = query.match(/What is (\d+) plus (\d+)/);
+    if(addMatch){
+      const x: number = parseInt(addMatch[1]);
+      const y: number = parseInt(addMatch[2]);
+      return (x+y).toString();
+    }
+    return (
+      "not match"
+    );
+  }
+
+  return "";
+}
